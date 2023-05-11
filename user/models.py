359c1f6b3ff7088e5b3d7634f4594d386f947f24
jumosd <jumosd@icloud.com>
2023-05-11 18:36:57 +0900

    기존 auth_user 테이블을 지우고 커스텀유저모델을 사용하게하기
    장고에서는 auth_user라는 기본 사용자 테이블이있지만
    커스텀해서 새로만들경우 사용자테이블이 두개가되면 안되므로
    settings.py 에서 유저인증모델을 정해주고
    models.py에서는 USERNAME_FIELD를 정해준다
    USERNAME_FIELD의 지정한 속성값은 유니크 해야한다

--- user/models.py
+++ user/models.py
@@ -10,10 +10,12 @@ class User(AbstractBaseUser):
     비번 = 원래있던거
     """
     profile_image = models.TextField()
-    nickname = models.CharField(max_length=24)
+    nickname = models.CharField(max_length=24, unique=True)
     name = models.CharField(max_length=24)
-    email = models.EmailField()
-    
+    email = models.EmailField(unique=True)
+
+    #커스텀 유저 를 생성시 유저네임필드가 있어야한다
+    USERNAME_FIELD = 'nickname'
     #db에 저장될때에는 앱이름_클래스이름으로 db테이블에 생성된다
     #user_User 테이블이 생기지만  Meta클래스로 테이블 이름을 변경 할수있다.
     class Meta:
