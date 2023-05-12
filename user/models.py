d1e09bdd5fc9ae3347491b5ee5aae2857d3376a3
jumosd <jumosd@icloud.com>
2023-05-12 13:32:12 +0900

    로그인화면 만들기 중간커밋

--- user/models.py
+++ user/models.py
@@ -2,13 +2,7 @@ from django.db import models
 from django.contrib.auth.base_user import AbstractBaseUser
 # Create your models here.
 class User(AbstractBaseUser):
-    """
-    프로필사진 =텍스트필드(텍스트필드는 길이무제한)
-    닉네임 = 캐릭터필드 24자
-    이름 =캐릭터필드 24자
-    이메일 =이메일필드
-    비번 = 원래있던거
-    """
+ 
     profile_image = models.TextField()
     nickname = models.CharField(max_length=24, unique=True)
     name = models.CharField(max_length=24)
--- user/views.py
+++ user/views.py
@@ -1,12 +1,35 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from rest_framework.views import APIView
+from django.urls import reverse
+from .models import User
+from django.contrib.auth.hashers import make_password
+
 # Create your views here.
 class Join(APIView):
     def get(self,request):
-
         return render(request, "user/join.html")
     
+    def post(self,request):
+        #회원 가입기능
+        email = request.data.get('email')
+        name =  request.data.get('name')
+        nickname = request.data.get('nickname')
+        password1 = request.data.get('password1')
+        password2 = request.data.get('password2')
+
+        if password1 == password2:
+            password = make_password(password1)
+            User.objects.create(email=email, name=name, nickname=nickname, password=password)
+            return redirect('user:login')
+        else:
+            pass
+
+
 class Login(APIView):
     def get(self,request):
-
+        return render(request, "user/login.html")
+    
+    def post(self,request):
+        #로그인 기능
+        #리다이렉트 메인페이지
         return render(request, "user/login.html")
