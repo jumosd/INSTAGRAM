936fb5745548f1ccba120eeb1ea38ac6d2096186
jumosd <jumosd@icloud.com>
2023-05-15 06:59:02 +0900

    장고 로그인정보 session 저장하는방법

--- content/views.py
+++ content/views.py
@@ -10,6 +10,7 @@ class Main(APIView):
     def get(self,request):
         feed_list = Feed.objects.all().order_by('-id') # 장고의 쿼리셋임  select * from content_feed;  와 같은동작을함
         
+        print(request.session['email'])
         context ={
             "feeds": feed_list
         }
--- user/views.py
+++ user/views.py
@@ -38,6 +38,7 @@ class Login(APIView):
 
         if user.check_password(password):
             #로그인을 했다 세션 or 쿠키
+            request.session['email'] = email
             return Response(status=200)
         else:
             return Response(status=400,data=dict(massage="로그인 정보가 잘못되었습니다."))
