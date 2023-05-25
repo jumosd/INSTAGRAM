b4139f524c553de5563901089a63551f09609bc8
jumosd <jumosd@icloud.com>
2023-05-25 09:23:20 +0900

    댓글 좋아요 북마크 모델 설정

--- content/models.py
+++ content/models.py
@@ -9,4 +9,18 @@ class Feed(models.Model):
     like_count = models.IntegerField() # 좋아요수
     user_email = models.EmailField()
     
-     
+class Like(models.Model):
+    feed_id = models.IntegerField(default=0)
+    email = models.EmailField(default='')
+    is_like = models.BooleanField(default=True)
+
+class Reply(models.Model):
+    feed_id = models.IntegerField(default=0)
+    email = models.EmailField(default='')
+    reply_content = models.TextField()
+
+class Bookmark(models.Model):
+    feed_id = models.IntegerField(default=0)
+    email = models.EmailField(default='')
+    is_bookmark = models.BooleanField(default=True)
+
